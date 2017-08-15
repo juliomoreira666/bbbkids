41cffa483297d03d2a12ee05695f8630c057ab92
Marcos D <dansouza91@outlook.com>
2017-08-15 11:18:10 -0300

    Correções de texto, e classe

--- dist/js/alteracao-de-texto.js
+++ dist/js/alteracao-de-texto.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 $('#newsletterButtonOK').val('CADASTRAR');
     
-    var text = document.querySelector("#NewsLetter_3e500739_4c49_4c6a_a2be_4a6050e6c6d1 > fieldset:nth-child(2) > p:nth-child(1)");
+    var text = document.querySelector(".newsletter p");
     var textPublique = document.querySelector("#lnkPubliqueResenha");
     var textAvaliacoes = document.querySelector("#resenha .user-review h4");
     
-    text.textContent = ("ASSINE NOSSA NEWSLETTER");
-    $(text).append("<p>Recebe nossas ofertas exclusivas!</p>");
+    text.textContent = ("ASSINE A NOSSA NEWSLETTER");
+    $(text).append("<p>Receba nossas ofertas exclusivas!</p>");
 
 
       textAvaliacoes.textContent = ("AVALIAÇÕES");
