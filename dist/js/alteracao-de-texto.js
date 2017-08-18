e78d81d636259874983149b1e450df2761f12102
Marcos D <dansouza91@outlook.com>
2017-08-18 13:31:40 -0300

    Correcoes menu hover

--- dist/js/alteracao-de-texto.js
+++ dist/js/alteracao-de-texto.js
@@ -1,38 +1,13 @@
 $(document).ready(function() {
-	$('.btn-roupas, .submenu-roupas').mouseenter(function(){
-		$('.submenu-roupas').css('display','flex');
-	});
-	$('.btn-roupas, .submenu-roupas').mouseleave(function(){
-		$('.submenu-roupas').css('display','none');
-	});
-	//CALÇADOS
-	$('.btn-calcados, .submenu-calcados').mouseenter(function(){
-		$('.submenu-roupas').css('display','flex');
-	});
-	$('.btn-calcados, .submenu-calcados').mouseleave(function(){
-		$('.submenu-roupas').css('display','none');
-	});
-	//ENXOVAL
-	$('.btn-enxoval, .submenu-enxoval').mouseenter(function(){
-		$('.submenu-enxoval').css('display','flex');
-	});
-	$('.btn-enxoval, .submenu-enxoval').mouseleave(function(){
-		$('.submenu-enxoval').css('display','none');
-	});
-	//QUARTO E BANHO
-	$('.btn-quarto-banho, .submenu-quarto-banho').mouseenter(function(){
-		$('.submenu-quarto-banho').css('display','flex');
-	});
-	$('.btn-quarto-banho, .submenu-quarto-banho').mouseleave(function(){
-		$('.submenu-quarto-banho').css('display','none');
-	});
-	//PASSEIO
-	$('.btn-passeio, .submenu-passeio').mouseenter(function(){
-		$('.submenu-passeio').css('display','flex');
-	});
-	$('.btn-passeio, .submenu-passeio').mouseleave(function(){
-		$('.submenu-passeio').css('display','none');
-	});
+$('#newsletterButtonOK').val('CADASTRAR');
+    
+    var text = document.querySelector(".newsletter p");
+    var textPublique = document.querySelector("#lnkPubliqueResenha");
+    var textAvaliacoes = document.querySelector("#resenha .user-review h4");
 
+    text.textContent = ("ASSINE A NOSSA NEWSLETTER");
+    $(text).append("<p>Receba nossas ofertas exclusivas!</p>");
 
+    textAvaliacoes.textContent = ("AVALIAÇÕES");
+    textPublique.textContent = ("FAZER UMA AVALIAÇÃO");
 });
--- dist/js/js.js
+++ dist/js/js.js
@@ -1,4 +1,39 @@
 $(document).ready(function() {
+  //MENU PRINCIPAL
+  $('.btn-roupas, .submenu-roupas').mouseenter(function(){
+    $('.submenu-roupas').css('display','flex');
+  });
+  $('.btn-roupas, .submenu-roupas').mouseleave(function(){
+    $('.submenu-roupas').css('display','none');
+  });
+  //CALÇADOS
+  $('.btn-calcados, .submenu-calcados').mouseenter(function(){
+    $('.submenu-roupas').css('display','flex');
+  });
+  $('.btn-calcados, .submenu-calcados').mouseleave(function(){
+    $('.submenu-roupas').css('display','none');
+  });
+  //ENXOVAL
+  $('.btn-enxoval, .submenu-enxoval').mouseenter(function(){
+    $('.submenu-enxoval').css('display','flex');
+  });
+  $('.btn-enxoval, .submenu-enxoval').mouseleave(function(){
+    $('.submenu-enxoval').css('display','none');
+  });
+  //QUARTO E BANHO
+  $('.btn-quarto-banho, .submenu-quarto-banho').mouseenter(function(){
+    $('.submenu-quarto-banho').css('display','flex');
+  });
+  $('.btn-quarto-banho, .submenu-quarto-banho').mouseleave(function(){
+    $('.submenu-quarto-banho').css('display','none');
+  });
+  //PASSEIO
+  $('.btn-passeio, .submenu-passeio').mouseenter(function(){
+    $('.submenu-passeio').css('display','flex');
+  });
+  $('.btn-passeio, .submenu-passeio').mouseleave(function(){
+    $('.submenu-passeio').css('display','none');
+  });
 
 var $showCaseOwlPrincipal = $('#owl-demo');
 var $showCaseOwlMarcas = $(".owl-carousel-2");
